a565a8d7d47f5c715401326c48d36c7404072da7
eddah73 <eddahsjr@gmail.com>
2024-10-05 20:41:46 +0300

    api

--- server/app.py
+++ server/app.py
@@ -20,19 +20,33 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakery_dicts = [bakery.to_dict() for bakery in bakeries]
+    response = make_response(bakery_dicts, 200)
+
+    return response
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+    response= make_response(bakery_dict, 200)
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
-
+    baked_goods= BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_dicts = [baked_good.to_dict() for baked_good in baked_goods]
+  
+    response= make_response(baked_goods_dicts, 200)
+    return response
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive= BakedGood.query.order_by(BakedGood.price.desc()).first()
+    most_expensive_dict =most_expensive.to_dict()
+    response = make_response(most_expensive_dict, 200)
+    return response
 
-if __name__ == '__main__':
+if __name__ == '__main_baked_goods = [baked_good for baked_good in BakedGood.query.all()]_':
     app.run(port=5555, debug=True)
